6db9a7557029e6512a5e08aa4ffa407ba56e8c3f
linporu <qazwsx123456guy@gmail.com>
2024-10-02 21:01:38 +0800

    Update export.py & .gitignore

---
+++ export.py
@@ -0,0 +1,17 @@
+import sqlite3
+import csv
+
+
+def main():
+    conn = sqlite3.connect('training.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM training')
+    rows = cursor.fetchall()
+    with open('training.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([i[0] for i in cursor.description])
+        writer.writerows(rows)
+
+
+if __name__ == "__main__":
+    main()
