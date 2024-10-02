05f847b69339336dd54aae1c61c7e9c882fd4a69
linporu <qazwsx123456guy@gmail.com>
2024-10-02 22:36:43 +0800

    Print message after export

--- export.py
+++ export.py
@@ -11,6 +11,7 @@ def main():
         writer = csv.writer(file)
         writer.writerow([i[0] for i in cursor.description])
         writer.writerows(rows)
+    print("Data exported to training.csv")
 
 
 if __name__ == "__main__":
