2604a30122cc1018988cc04eb9f46af51b6f4b22
linporu <qazwsx123456guy@gmail.com>
2024-10-02 22:23:43 +0800

    Bugfix: weight, reps, sets, volume can be None

--- clean.py
+++ clean.py
@@ -1,60 +1,56 @@
 import re
 
 
+WEIGHT_PATTERN = r'(\d+(?:\.\d+)?)\s*kg ?(?: x2)?'
+REPS_PATTERN = r'(\d+)\s*下'
+SETS_PATTERN = r'(\d+)\s*組'
+
+
 def clean_position(s: str) -> str:
-    """
-    Clean up the position name to match the database
-    """
     valid_positions = ["胸", "背", "腿", "肩", "手", "核心"]
     if s.startswith('## '):
-            position = s.strip('# ')  # Position 句子
-            if position[0:1] in valid_positions:
-                position = position[0:1]  # Position 第一個字
-            elif position[0:2] in valid_positions:
-                position = position[0:2]  # Position 前兩個字，例如核心
-            else:
-                position = 'Unknown'
+        position = s.strip('# ')  # Position 句子
+        if position[0:1] in valid_positions:
+            position = position[0:1]  # Position 第一個字
+        elif position[0:2] in valid_positions:
+            position = position[0:2]  # Position 前兩個字，例如核心
+        else:
+            position = None
     return position
 
 
-def clean_exercise(s: str) -> str:
-    """
-    Clean up the exercise name to match the database
-    """
-    position = s.strip()
+def clean_exercise(s: str) -> str | None:
+    if '｜' not in s:
+        return None
+    
+    exercise = s.split('｜')[0].strip()
 
     # Replace 'Leg Crul' with 'Leg Curl'
-    if 'Leg Crul' in position:
-        position = position.replace('Leg Crul', 'Leg Curl')
+    if 'Leg Crul' in exercise:
+        exercise = exercise.replace('Leg Crul', 'Leg Curl')
 
-    return position
-
-
-def clean_weight(s: str) -> float:
-    """
-    Clean up the weight to match the database
-    """
-    weight_match = re.search(r'(\d+(?:\.\d+)?)\s*kg', s)
-    weight = round(float(weight_match.group(1)), 1)  # Weight
-    if 'x2' in s:  # Single-hand exercise
-        return round(weight * 2, 1)
-    else:
-        return weight
+    return exercise if exercise else None
 
 
-def clean_reps(s: str) -> int:
-    """
-    Clean up the reps to match the database
-    """
-    reps_match = re.search(r'(\d+)\s*下', s)
-    reps = int(reps_match.group(1))  # Reps
-    return reps
+def clean_weight(s: str) -> float | None:
+    if s is None:
+        return None
+    weight_match = re.search(WEIGHT_PATTERN, s)
+    if weight_match:
+        weight = round(float(weight_match.group(1)), 1)
+        return weight if not 'x2' in s else round(weight * 2, 1)  # single-hand exercise
+    return None
 
 
-def clean_sets(s: str) -> int:
-    """
-    Clean up the sets to match the database
-    """
-    sets_match = re.search(r'(\d+)\s*組', s)
-    sets = int(sets_match.group(1))  # Sets
-    return sets
+def clean_reps(s: str) -> int | None:
+    if s is None:
+        return None
+    reps_match = re.search(REPS_PATTERN, s)
+    return int(reps_match.group(1)) if reps_match else None
+
+
+def clean_sets(s: str) -> int | None:
+    if s is None:
+        return None
+    sets_match = re.search(SETS_PATTERN, s)
+    return int(sets_match.group(1)) if sets_match else None
--- parse.py
+++ parse.py
@@ -1,50 +1,37 @@
+from clean import clean_position, clean_exercise, clean_weight, clean_reps, clean_sets
 import re
 from datetime import datetime
-from clean import clean_position, clean_exercise, clean_weight, clean_reps, clean_sets
 
 def parse_workout_log(file_name: str, content: list[str]) -> list[dict]:
-
     # Parse date
     date_match = re.search(r'(\d{6})', file_name)
     if date_match:
         date_str = date_match.group(1)
         date = datetime.strptime(date_str, '%y%m%d').strftime('%Y-%m-%d')
     else:
-        date = "Unknown"
+        date = None
 
-    # Parse exercise data
     exercises: list[dict] = []
     for line in content:
         if line.startswith('## '):
             position = clean_position(line)
         elif line.startswith('- [x] '):
-            exercise_data = line[6:].split('｜')
-            if len(exercise_data) > 0:
-                exercise: str | None = get_value(exercise_data, 0, clean_exercise)
-                weight: float | None = get_value(exercise_data, 1, clean_weight)
-                reps: int | None = get_value(exercise_data, 2, clean_reps)
-                sets: int | None = get_value(exercise_data, 3, clean_sets)
+            exercise_data = line[6:].strip()
 
-                # Calculate volume, only calculate when all necessary values exist
-                if weight is not None and reps is not None and sets is not None:
-                    volume: float = round(weight * reps * sets, 1)
-                else:
-                    volume = None
+            exercise = clean_exercise(exercise_data)
+            weight = clean_weight(exercise_data)
+            reps = clean_reps(exercise_data)
+            sets = clean_sets(exercise_data)
+            volume = round(weight * reps * sets, 1) if all([weight, reps, sets]) else None
 
-                exercises.append({
-                    'date': date,
-                    'position': position,
-                    'exercise': exercise,
-                    'weight': weight,
-                    'reps': reps,
-                    'sets': sets,
-                    'volume': volume
-                })
+            exercises.append({
+                'date': date,
+                'position': position,
+                'exercise': exercise,
+                'weight': weight,
+                'reps': reps,
+                'sets': sets,
+                'volume': volume
+            })
 
     return exercises
-
-
-def get_value(data: list[str], index: int, clean_func: callable) -> str | None:
-    if index < len(data):
-        return clean_func(data[index])
-    return None
